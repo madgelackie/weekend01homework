922e24d6eaa2b3bfb8a32b6a7381cfd04a97f8d3
madgelackie <marionmlackie@gmail.com>
2021-09-10 21:34:25 +0100

    solution2

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,2 +1,5 @@
-def get_pet_shop_name(shop_name):
-    return (shop_name["name"])
+def get_pet_shop_name(shop_list):
+    return (shop_list["name"])
+
+def get_total_cash(shop_list):
+    return (shop_list["admin"]["total_cash"])
