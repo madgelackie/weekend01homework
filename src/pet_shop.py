3e6c20e0db1bc4205e10a05c5980e263f082e2d6
madgelackie <marionmlackie@gmail.com>
2021-09-10 22:09:26 +0100

    I_did_it_yipee_solution3

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -3,3 +3,7 @@ def get_pet_shop_name(shop_list):
 
 def get_total_cash(shop_list):
     return (shop_list["admin"]["total_cash"])
+
+def add_or_remove_cash(shop_list, add_cash):
+    updated_cash = get_total_cash(shop_list) + add_cash
+    shop_list["admin"]["total_cash"] = updated_cash
